1e5f19818fa010e0bf8f3da4016e193043af38eb
Shawn Connolly <sconnolly@unicon.net>
2013-09-04 14:41:59 -0700

    WPP-69 - Updating to show error message in UI if encryption error occurs - Adding catch all error page (mimicing emailplt) - Updating comment in gateway config to indicate a bean that is not a part of the zimbra example
    - Change security class to match EmailPreview's implementation
    - Catches when encryption key is at default and logs ERROR message with no stack trace
    - Exposes boolean indicating encryption key is at default value
    - Uses annotation for bean initialization

--- src/main/java/org/jasig/portlet/proxy/security/JasyptPBEStringEncryptionServiceImpl.java
+++ src/main/java/org/jasig/portlet/proxy/security/JasyptPBEStringEncryptionServiceImpl.java
@@ -21,10 +21,13 @@ package org.jasig.portlet.proxy.security;
 
 import org.jasypt.encryption.pbe.PBEStringEncryptor;
 import org.jasypt.exceptions.EncryptionInitializationException;
+import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.util.Assert;
+
+import javax.annotation.PostConstruct;
 
 /**
  * JasyptPBEStringEncryptionServiceImpl is an implementation of 
@@ -33,72 +36,89 @@ import org.springframework.util.Assert;
  * 
  * @author Jen Bourey
  */
-public class JasyptPBEStringEncryptionServiceImpl implements IStringEncryptionService, InitializingBean {
+public class JasyptPBEStringEncryptionServiceImpl implements IStringEncryptionService {
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	private PBEStringEncryptor encryptor = null;
-	
-	/**
-	 * Set the PBEStringEncryptor to be used
-	 * 
-	 * @param encryptor
-	 */
-	public void setStringEncryptor(final PBEStringEncryptor encryptor) {
-		this.encryptor = encryptor;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public String encrypt(final String plaintext) {
-		if (this.encryptor == null) {
-			logger.error("encryptor not set");
-            return null;
-		}
+    private PBEStringEncryptor encryptor = null;
+    
+    /**
+     * Set the PBEStringEncryptor to be used
+     * 
+     * @param encryptor
+     */
+    @Required
+    public void setStringEncryptor(final PBEStringEncryptor encryptor) {
+        this.encryptor = encryptor;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public String encrypt(final String plaintext) {
         try {
             return this.encryptor.encrypt(plaintext);
         } catch (EncryptionInitializationException e) {
             logger.warn(e.getMessage(), e);
             throw new StringEncryptionException("Encryption error. Verify an encryption password"
-                    + " is configured in the email preview portlet's"
-                    + " stringEncryptionService bean in applicationContent.xml", e);
+                    + " is configured in configuration.properties", e);
         }
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public String decrypt(final String cryptotet) {
-		if (this.encryptor == null) {
-			logger.error("encryptor not set");
-            return null;
-		}
-      try {
-	        return this.encryptor.decrypt(cryptotet);
-      } catch (EncryptionInitializationException e) {
-    	  logger.warn(e.getMessage(), e);
-          throw new StringEncryptionException("Decryption error. Was encryption password"
-                  + " changed in the email preview portlet's"
-                  + " stringEncryptionService bean in applicationContent.xml?", e);
-      }
+    }
+    
+    private String decrypt(final String cryptotext, boolean logError) {
+        try {
+            return this.encryptor.decrypt(cryptotext);
+        } catch (EncryptionInitializationException e) {
+            if (logError) {
+                logger.warn("Decryption failed.  Error message: {}", e.getMessage());
+            }
+            throw new StringEncryptionException("Decryption error. Was encryption password"
+                        + " changed in the configuration.properties?", e);
+        } catch (EncryptionOperationNotPossibleException e) {
+            if (logError) {
+                logger.warn("Decryption failed.  This suggests the salt key in "
+                        + "configuration.properties has been changed.");
+            }
+            throw new StringEncryptionException("Decryption error. Was encryption password"
+                        + " changed in the configuration.properties?", e);
+        }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
+    public String decrypt(final String cryptotext) {
+        return decrypt(cryptotext, true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @PostConstruct
     public void afterPropertiesSet() throws Exception {
         final String enc = this.encrypt(this.getClass().getName());
         Assert.notNull(enc, "String encryption service is not properly configured.");
-        
+
         final String dec = this.decrypt(enc);
         Assert.notNull(dec, "String decryption service is not properly configured.");
         Assert.isTrue(dec.equals(this.getClass().getName()), "String decryption failed to decode the encrypted text " + enc);
+
         if (usingDefaultEncryptionKey()) {
             logger.error("Encryption key at default value.  Change it in configuration.properties for improved security!");
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean usingDefaultEncryptionKey() {
-        String result = this.decrypt("pD2vrJ0CiAbnW4k4lF84S8yXN6gSl6VUjISd8NN6AFnDGuei5rGyuw==");
-        return "EncryptionKeyStillchangeMe".equals(result);
+        try {
+            String result = this.decrypt("pD2vrJ0CiAbnW4k4lF84S8yXN6gSl6VUjISd8NN6AFnDGuei5rGyuw==", false);
+            return "EncryptionKeyStillchangeMe".equals(result);
+        } catch (StringEncryptionException e) {
+            // If encryption is being used, a decryption error is good because it means the encryption key was changed.
+            return false;
+        }
     }
 }
